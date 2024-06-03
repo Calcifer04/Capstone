da0896c94b08ce43eadb5890a14ade98f04841f2
Calcifer04 <sebastian.vanschalkwyk@gmail.com>
2024-06-03 20:20:42 +0200

    Sphinx Documentation Added

--- mySite/personal/views.py
+++ mySite/personal/views.py
@@ -4,14 +4,35 @@ from django.shortcuts import render
 # VIEWS
 # Home view
 def home(request):
+    """ Used in urls.py to show home.html upon empty url path.
+        Handles Http requests & renders home.html in response.
+    :param request: HttpRequest
+    :type request: Any
+    :return: Rendered view of home.html
+    :rtype: HttpResponse
+    """
     return render(request, "home.html")
 
 
 # CV view
 def my_cv(request):
+    """Used in urls.py to show cv.html upon 'cv/' url path specification.
+        Handles Http requests & renders cv.html in response.
+    :param request: HttpRequest
+    :type request: Any
+    :return: Rendered view of cv.html
+    :rtype: HttpResponse
+    """
     return render(request, "cv.html")
 
 
 # Contact me view
 def contactme(request):
+    """Used in urls.py to show contactme.html upon 'contactme/' url path specification.
+        Handles Http requests & renders contactme.html in response.
+    :param request: HttpRequest
+    :type request: Any
+    :return: Rendered view of contactme.html
+    :rtype: HttpResponse
+    """
     return render(request, "contactme.html")
--- mySite/user_auth/forms.py
+++ mySite/user_auth/forms.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 
 # Registration details class model.
 class SignupForm(UserCreationForm):
+    """Class model for signup form with username, password and password reconfirmation fields.
+    :param UserCreationForm: Inheritance of the django form class
+    """
     class Meta:
         model = User
         fields = ['username', 'password1', 'password2']
