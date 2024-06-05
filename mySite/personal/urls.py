a40f2d994d394a14e81b5fff1f8226041e7a1c9f
Calcifer04 <sebastian.vanschalkwyk@gmail.com>
2024-06-05 12:48:29 +0200

    Finalized3

--- mySite/personal/urls.py
+++ mySite/personal/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 # Mapping url path patterns to views
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name='home'),
     path('cv/', views.my_cv),
     path('contactme/', views.contactme)
 ]
